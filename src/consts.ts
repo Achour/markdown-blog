882c254fb190687bfb502bec6429ddb4a92e35cd
Meguenni Achour <achourphp@gmail.com>
2023-10-07 17:59:59 +0100

    added logo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Achour.Dart | Blog';
 export const SITE_DESCRIPTION = 'A blog by a passionate tech enthusiast and a self-taught web developer.';
