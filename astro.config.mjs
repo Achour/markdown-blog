d42b086ad0c38bd35a96f34177cf2cddbea87cc7
Meguenni Achour <achourphp@gmail.com>
2023-10-07 18:12:23 +0100

    deleted astro analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+
   site: 'https://achourdart.vercel.com',
   integrations: [mdx(), sitemap(), tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
