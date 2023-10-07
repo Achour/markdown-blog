5b546ec9e4498e4343c4bfc08edeb392cb71fa33
Meguenni Achour <achourphp@gmail.com>
2023-10-07 18:07:02 +0100

    added astro analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  site: 'https://achourdart.vercel.com',
   integrations: [mdx(), sitemap(), tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
