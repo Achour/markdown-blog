16b32f1e475463fc5aa03d71fe5df96405bc1e0d
Meguenni Achour <achourphp@gmail.com>
2023-10-07 18:28:20 +0100

    fixed post.render is not a function

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://achourdart.vercel.com',
+  site: 'https://achourdart.vercel.com/',
   integrations: [mdx(), sitemap(), tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
